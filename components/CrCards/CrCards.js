6e4e9c4ca5e407ab9eac49d055c7baa42a23e854
jameelappsintegra <mail4jameelmca@gmail.com>
2021-06-13 21:59:12 +0400

    Changes

--- components/CrCards/CrCards.js
+++ components/CrCards/CrCards.js
@@ -1,42 +1,41 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+const cardsData = [
+  {id:'1' ,imgUrl: "../../assets/images/fitness.jpg", title: "Fitness" },
+  {id:'2' ,imgUrl: "../../assets/images/fitness.jpg", title: "Learning" },
+  {id:'3' ,imgUrl: "../../assets/images/fitness.jpg", title: "Cooking" },
+];
 
 export default function CrCards() {
-    return (
-        <View style={{ flexDirection: 'row', justifyContent: 'space-around', width: '100%' }}>
-            <View>
-                <Image
-                    source={require('../../assets/images/fitness.jpg')}
-                    style={{
-                        height: 135,
-                        width: 120, borderRadius: 10
-                    }}
-                />
-                <Text style={{ position: 'absolute', bottom: 10, left: 10 }}>Title</Text>
-            </View>
-
-            <View>
-                <Image
-                    source={require('../../assets/images/image1.png')}
-                    style={{
-                        height: 135,
-                        width: 120, borderRadius: 10
-                    }}
-                />
-                <Text style={{ position: 'absolute', bottom: 10, left: 10 }}>Title</Text>
-            </View>
-            <View>
-                <Image
-                    source={require('../../assets/images/image1.png')}
-                    style={{
-                        height: 135,
-                        width: 120, borderRadius: 10
-                    }}
-                />
-                <Text style={{ position: 'absolute', bottom: 10, left: 10 }}>Title</Text>
-            </View>
-        </View>
-    )
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-around",
+        width: "100%",
+      }}
+    >
+      {cardsData.map((item) => {
+        return (
+          
+          <View key={item.id}>
+            <Image
+              source={require("../../assets/images/cooking.jpg")}
+              style={{
+                height: 135,
+                width: 120,
+                borderRadius: 10,
+              }}
+            />
+            <Text style={{ position: "absolute", bottom: 10, left: 10 }}>
+              {item.title}
+            </Text>
+          </View>
+        );
+      })}
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
